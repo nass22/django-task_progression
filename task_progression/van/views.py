2aaafca39459b91832981a10316c7ee9a7d0a71e
Nacer Samir <84498447+nass22@users.noreply.github.com>
2023-03-21 10:52:20 +0100

    view change

--- task_progression/van/views.py
+++ task_progression/van/views.py
@@ -3,11 +3,15 @@ from django.shortcuts import render
 from van.models import Van, Work_tracking
 
 # Create your views here.
+
+
 def tracking(request, tracking_id):
-    
-    van_tracking = Work_tracking.objects.get(tracking_id=tracking_id);
+
+    van = Van.objects.get(tracking_id=tracking_id)
+    work_tracking = Work_tracking.objects.filter(van=van).order_by('-created_at')
     # van = Van.objects.filter(id=van_tracking.van.id)
-    
+
     return render(request,
                   'van/tracking.html',
-                  {'van_tracking': van_tracking})
+                  {'van': van,
+                   'work_tracking': work_tracking})
