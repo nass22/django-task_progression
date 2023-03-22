c4819176b72e954c21c8b0b8e0be08544742da17
Nacer Samir <84498447+nass22@users.noreply.github.com>
2023-03-22 16:17:33 +0100

    view + html edit

--- task_progression/van/views.py
+++ task_progression/van/views.py
@@ -7,7 +7,7 @@ from van.models import Van, Work_tracking, Img_tracking, Work_requested
 def tracking(request, tracking_id):
 
     van = Van.objects.get(tracking_id=tracking_id)
-    work_requested = Work_requested.objects.filter(van=van).order_by('-created_at')
+    work_requested = Work_requested.objects.filter(van=van)
     work_tracking = Work_tracking.objects.filter(van=van).order_by('-created_at')
     img_tracking = Img_tracking.objects.filter(van=van).order_by('-created_at')
 
